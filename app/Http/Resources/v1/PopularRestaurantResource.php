7aac7a22f926965b792909e87e77dc5c5c94c905
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-12-23 00:28:34 +0300

    popular

--- app/Http/Resources/v1/PopularRestaurantResource.php
+++ app/Http/Resources/v1/PopularRestaurantResource.php
@@ -13,21 +13,26 @@ class PopularRestaurantResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
 
         return [
-            "id"              => $this->id,
-            "name"            => $this->name,
-            "description"     => strip_tags($this->description),
-            "address"         => $this->address,
-            "image"           => $this->image,
-            "avgRating"       => $this->avgRatings['avgRating'],
-            "avgRatingUser"   => $this->avgRatings['countUser'],
-
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => strip_tags($this->description),
+            "lat" => $this->lat,
+            "long" => $this->long,
+            "opening_time" => $this->opening_time,
+            "closing_time" => $this->closing_time,
+            "temporary_closed_until" => $this->temporary_closed_until,
+            "permanently_closed" => $this->permanently_closed,
+            "address" => $this->address,
+            "image" => $this->image,
+            "avgRating" => $this->avgRatings['avgRating'],
+            "avgRatingUser" => $this->avgRatings['countUser'],
         ];
     }
 }
