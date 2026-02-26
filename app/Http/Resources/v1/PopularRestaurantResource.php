a8fd59fc86c387806982ef847bf02dd1f339a720
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2026-02-26 14:19:26 +0300

    badge

--- app/Http/Resources/v1/PopularRestaurantResource.php
+++ app/Http/Resources/v1/PopularRestaurantResource.php
@@ -28,6 +28,7 @@ class PopularRestaurantResource extends JsonResource
             "avgRating" => $this->avgRatings['avgRating'],
             "avgRatingUser" => $this->avgRatings['countUser'],
             "isOpen" => $isOpen,
+            "isOpenBadge" => $isOpen ? 'Şimdi Açık' : 'Şu an Kapalı',
             "isOpenMessage" => $isOpen ? 'Şimdi Açık' : RestaurantHelper::getStatusMessage($this->resource),
         ];
     }
