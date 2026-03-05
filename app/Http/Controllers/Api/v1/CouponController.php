2995fc373ae2575f33180ff27f79a724b95ad105
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2026-03-05 22:28:26 +0300

    couponş

--- app/Http/Controllers/Api/v1/CouponController.php
+++ app/Http/Controllers/Api/v1/CouponController.php
@@ -14,6 +14,7 @@ use App\Models\Discount;
 use App\Traits\ApiResponse;
 use App\Models\Banner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Schema;
 
 class CouponController extends BackendController
@@ -32,32 +33,71 @@ class CouponController extends BackendController
      */
     public function apply(Request $request)
     {
-
         $msg = '';
-        $today = date('Y-m-d h:i:s');
-        $coupon = Coupon::where('slug', $request->coupon)->whereDate('to_date', '>', $today)
-            ->where('restaurant_id', '=', $request->restaurantID)
-            ->whereDate('from_date', '<', $today)
-            ->where('limit', '>', 0)->first();
+        $discountAmount = 0;
+        $couponID = 0;
+
+        $today = Carbon::now()->format('Y-m-d H:i:s');
+        $total_amount = $request->total_amount;
+        $restaurant_id = $request->restaurant_id;
+
+        $coupon = Coupon::where('slug', $request->coupon)->first();
 
         if (!blank($coupon)) {
-            $total_used = Discount::where('coupon_id', $coupon->id)->where('status', DiscountStatus::ACTIVE)->count();
+            $total_used = Discount::where('coupon_id', $coupon->id)
+                ->where('status', DiscountStatus::ACTIVE)
+                ->count();
+
+            $user_limit = Discount::where([
+                'coupon_id' => $coupon->id,
+                'user_id' => auth()->id() ?? 0
+            ])
+                ->where('status', DiscountStatus::ACTIVE)
+                ->count();
+
+            if ($coupon->coupon_type == CouponType::VOUCHER && $coupon->restaurant_id != $restaurant_id) {
+                $msg = 'Bu kupon bu restoran için geçerli değil';
+            } elseif ($total_used >= $coupon->limit) {
+                $msg = 'Tutar limitten eşit ve büyük olamaz';
+            } elseif (!(($coupon->to_date >= $today) && ($coupon->from_date <= $today))) {
+                $msg = 'Bu Kuponun Süresi Doldu';
+            } elseif ($user_limit >= $coupon->user_limit) {
+                $msg = 'Limitiniz kupon limitinden büyük olamaz';
+            } elseif ($total_amount < $coupon->minimum_order_amount) {
+                $msg = 'Bu Kupon için Minimum Sipariş Tutarı ' . currencyFormat($coupon->minimum_order_amount);
+            } elseif ($coupon->user_id != null && $coupon->user_id != auth()->id()) {
+                $msg = 'Üzgünüz, bu kupon sadece belirli müşteriler için geçerlidir.';
+            }
+        } else {
+            $msg = 'Bu Kupon Geçersiz';
         }
 
-        if (blank($coupon)) {
-            $msg = 'Bu Kupon Geçersiz';
-        } elseif ($coupon->coupon_type == CouponType::VOUCHER && $coupon->restaurant_id != $request->restaurantID) {
-            $msg = 'Bu Kupon Geçersiz';
-        } elseif ($total_used >= $coupon->limit) {
-            $msg = 'Bu Kuponun Süresi Doldu';
-        } else {
-            $msg = '';
+        if (blank($msg) && !blank($coupon)) {
+            $couponID = $coupon->id;
+
+            if ($coupon->discount_type == DiscountType::FIXED) {
+                $discountAmount = round($coupon->amount);
+            } else {
+                $discountAmount = round(($total_amount * $coupon->amount) / 100);
+            }
+
+            if ($coupon->user_id != null) {
+                $coupon->delete();
+            }
+
+            return $this->successresponse([
+                'success' => true,
+                'coupon_id' => $couponID,
+                'discount_amount' => $discountAmount,
+                'new_total' => $total_amount - $discountAmount,
+                'msg' => 'Kupon başarıyla uygulandı'
+            ], 200);
         }
 
-        if (blank($msg)) {
-            return $this->successresponse(['success' => 'coupon', 'msg' => $msg, 'coupon' => $coupon], 200);
-        } else {
-            return $this->successresponse(['success' => 'coupon', 'msg' => $msg, 'coupon' => $coupon], 422);
-        }
+        return $this->successresponse([
+            'success' => false,
+            'msg' => $msg,
+            'discount_amount' => 0
+        ], 422);
     }
 }
